2da0de0ce1b1f1a87df4c9acc23eccb9cbba4362
lpp <2107467756@qq.com>
2017-03-29 16:25:15 +0800

    为注册逻辑添加ButterKnife

---
+++ SuperWeChat/src/cn/ucai/superwechat/db/IUserModel.java
@@ -0,0 +1,13 @@
+package cn.ucai.superwechat.db;
+
+import android.content.Context;
+
+/**
+ * Created by LPP on 2017/3/29.
+ */
+
+public interface IUserModel {
+    public void register(Context context, String userName, String userNick, String password, OnCompleteListener<String> completeListener);
+    public void login(Context context,String userName,String password,OnCompleteListener<String> completeListener);
+    public void unRegister(Context context,String userName,OnCompleteListener<String> onCompleteListener);
+}
---
+++ SuperWeChat/src/cn/ucai/superwechat/db/UserModel.java
@@ -0,0 +1,43 @@
+package cn.ucai.superwechat.db;
+
+import android.content.Context;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.utils.OkHttpUtils;
+
+/**
+ * Created by LPP on 2017/3/29.
+ */
+
+public class UserModel implements IUserModel {
+    @Override
+    public void register(Context context, String userName, String userNick, String password, OnCompleteListener<String> completeListener) {
+        OkHttpUtils<String> okHttpUtils = new OkHttpUtils(context);
+        okHttpUtils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.NICK,userNick)
+                .addParam(I.User.PASSWORD,password)
+                .post()
+                .targetClass(String.class)
+                .execute(completeListener);
+    }
+
+    @Override
+    public void login(Context context, String userName, String password, OnCompleteListener<String> completeListener) {
+        OkHttpUtils<String> okHttpUtils = new OkHttpUtils<>(context);
+        okHttpUtils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.PASSWORD,password)
+                .targetClass(String.class)
+                .execute(completeListener);
+    }
+
+    @Override
+    public void unRegister(Context context, String userName, OnCompleteListener<String> completeListener) {
+        OkHttpUtils<String> okHttpUtils = new OkHttpUtils<>(context);
+        okHttpUtils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME,userName)
+                .targetClass(String.class)
+                .execute(completeListener);
+    }
+}
